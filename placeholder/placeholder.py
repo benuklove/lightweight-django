3c93c81ae1c52aadb18e41adbc7b56f73eaafd5b
benlove <benuklove@gmail.com>
2018-03-30 18:05:27 -0500

    Add placeholder view and url route with form validation

--- placeholder/placeholder.py
+++ placeholder/placeholder.py
@@ -22,14 +22,33 @@ settings.configure(
 )
 
 from django.conf.urls import url
+from django import forms
 from django.core.wsgi import get_wsgi_application
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 
+
+class ImageForm(forms.Form):
+    """Form to validate requested placeholder image."""
+    height = forms.IntegerField(min_value=1, max_value=2000)
+    width = forms.IntegerField(min_value=1, max_value=2000)
+
+
+def placeholder(request, width, height):
+    form = ImageForm({'height': height, 'width': width})
+    if form.is_valid():
+        height = form.cleaned_data['height']
+        width = form.cleaned_data['width']
+        # TODO: Generate image of requested size
+        return HttpResponse('Ok')
+    else:
+        return HttpResponseBadRequest('Invalid Image Request')
 
 def index(request):
     return HttpResponse('Hello World')
 
-urlpatterns = (url(r'^$', index),
+urlpatterns = (
+    url(r'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder, name='placeholder'),
+    url(r'^$', index, name='homepage'),
 )
 
 application = get_wsgi_application()
